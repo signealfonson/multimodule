78b2e1a053c7c0674b2c07dfc8911a5a1f8b8cd3
Signe <signe.alfonson@hotmail.com>
2023-04-20 10:19:34 +0200

    add Service Providers

---
+++ consumer/src/main/java/org/example/consumer/Consumer.java
@@ -0,0 +1,10 @@
+package org.example.consumer;
+
+import org.example.service.Greeting;
+
+public class Consumer {
+    public static void main(String[] args) {
+
+        Greeting greeting;
+    }
+}
